353502cd7b53341c793caf1a9723d270998e53f8
JeffMahou <jeffrey.mahou@eleves.enpc.fr>
2021-05-06 15:09:40 +0200

    preprocess

--- Solution.py
+++ Solution.py
@@ -1,8 +1,6 @@
 import utils
 import config as CONF
 
-file = "C:/Users/jeffm/Desktop/Cours/kiro/kiro2021/data/sujet4/instances/A.json"
-
 class Solution:
     def __init__(self, file):
         self.name = file
@@ -30,12 +28,39 @@ class Solution:
                 score += CONF.C0
         return score
 
+    def compute_admissible(self):
+
+        self.admissible = {}
+        for group_train in self.trains:
+            interdites = []
+            for train in group_train:
+
+                for inter in self.interdictions:
+                    if train["voieEnLigne"] in inter["voiesEnLigne"] or intersection(inter["typesMateriels"], train["typesMateriels"])>0 or train["typeCirculation"] in inter["typesCirculation"]:
+                        interdites.extend(inter["voiesAQuaiInterdites"])
+            interdites = list(set(interdites))
+
+            for train in group_train:
+                self.admissible[train["id"]] = []
+                for itin in self.itineraires:
+                    if itin["sensDepart"] == train["sensDepart"]:
+                        if itin["voieEnLigne"] == train["voieEnLigne"]:
+                            if itin["voieAQuai"] not in interdites:
+
+                                self.admissible[train["id"]].append(itin["id"])
+
+
+
     def export(self):
 
         with open('A.json', 'w') as outfile:
             json.dump(self.solution, outfile)
 
+def intersection(lst1, lst2):
+    lst3 = [value for value in lst1 if value in lst2]
 
+    return len(lst3)
 
-S = Solution(file)
-S.export()
+sol = Solution(CONF.file)
+sol.compute_admissible()
+
